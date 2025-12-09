2cbd27c5077ed4e0f8c83a9cde40d7444c517aa2
Binay <binayuppensharma@gmail.com>
2025-12-09 12:42:17 +0530

    fix

--- taskflows-frontend/src/main.jsx
+++ taskflows-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import bgImage from "../../assets/images/bkg30.jpg";
+import bgImage from "../assets/images/bkg30.jpg";
 
 const DashboardLayout = ({ children }) => {
   return (
